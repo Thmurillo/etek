53c035539f10bafa50509cadc0963c4467383c87
Thales Murillo <thmurillo@hotmail.com>
2025-05-12 00:28:55 -0300

    Primeiro commit

---
+++ store/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Device(models.Model):
+    name = models.CharField(max_length= 50, unique = True, null= False)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Accessory(models.Model):
+    name = models.CharField(max_length=50,unique=True, null=False)
+    price = models.DecimalField(max_digits= 12, decimal_places=2)
+    description = models.CharField(max_length=100)
+    devices = models.ManyToManyField(Device, related_name= 'accessories')
+
+    def __str__(self):
+        return self.name
+
